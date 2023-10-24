cb9fe7bdedbbb104d226e66a236774f663f275a2
lmbzib0 <bryan.zimmerlin@liebherr.com>
2023-10-24 07:20:24 +0200

    remove minimal name for our recipe image

--- sources/build-image.sh
+++ sources/build-image.sh
@@ -8,4 +8,4 @@ cp -r $HOME/sources/meta-watering-can-robot /workdir/$POKY_BRANCH
 
 source $POKY_BRANCH/oe-init-build-env $HOME/build
 
-bitbake watering-can-robot-minimal-image
+bitbake watering-can-robot-image
